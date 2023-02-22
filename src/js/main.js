688e8125349aae0627d81218d45692c1d3e62e50
Mateus Alves <contatomateusalves@hotmail.com>
2023-02-22 12:12:36 -0300

    refactor: zero Formatter and clean code

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+import Countdown from "./countdown.js";
+
+const date = "31 Dec 2023 23:59:59 GMT-0300";
+
+const days = document.querySelector("#day");
+const hours = document.querySelector("#hour");
+const minutes = document.querySelector("#minute");
+const seconds = document.querySelector("#second");
+
+function initTimer(date) {
+  const validity = new Countdown(date);
+
+  function zeroFormatter(time) {
+    return validity.total[time] < 10
+      ? `0${validity.total[time]}`
+      : validity.total[time];
+  }
+
+  days.innerText = zeroFormatter("days");
+  hours.innerText = zeroFormatter("hours");
+  minutes.innerText = zeroFormatter("minutes");
+  seconds.innerText = zeroFormatter("seconds");
+}
+
+setInterval(initTimer, 1000, date);
+initTimer(date);
