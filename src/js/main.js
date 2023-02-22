3852dd3a4c10d614a02c951ffab1299b37941e4f
Mateus Alves <contatomateusalves@hotmail.com>
2023-02-22 13:58:54 -0300

    refactor: optional validity and auto update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
 import Countdown from "./Countdown.js";
 
-const date = "31 Dec 2023 23:59:59 GMT-0300";
+let inputDate = document.querySelector("#date");
 
 const days = document.querySelector("#day");
 const hours = document.querySelector("#hour");
 const minutes = document.querySelector("#minute");
 const seconds = document.querySelector("#second");
+
+const actualDate = new Date();
+
+function addDays(date, days) {
+  date.setDate(date.getDate() + days);
+  return date;
+}
+const newDate = addDays(actualDate, 7);
+
+let date = newDate.toISOString().slice(0, 10);
+inputDate.setAttribute("min", date);
+inputDate.value = date;
 
 function initTimer(date) {
   const validity = new Countdown(date);
@@ -22,5 +34,17 @@ function initTimer(date) {
   seconds.innerText = zeroFormatter("seconds");
 }
 
-setInterval(initTimer, 1000, date);
+let interval = setInterval(initTimer, 1000, date);
+
+function updateInterval() {
+  clearInterval(interval);
+  date = inputDate.value;
+  console.log(date);
+
+  initTimer(date);
+  interval = setInterval(initTimer, 1000, date);
+}
+
+inputDate.addEventListener("input", updateInterval);
+
 initTimer(date);
